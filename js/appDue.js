00d61998f730a9f437a1a305e39cddd412c428cd
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-09 16:57:58 +0200

    app js

--- js/appDue.js
+++ js/appDue.js
@@ -3,12 +3,15 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-var machineNumber=Math.floor(Math.random()* 7);
+
+    
+
+var machineNumber=Math.floor(Math.random()* 7);     // Numeri randomici dei due giocatori giocatore vs computer
 var playerNumber=Math.floor(Math.random() *7);
-var confronto;
+var confronto;                                      // Esito del confronto fra i due giocatori
 
 
-if(machineNumber > playerNumber){
+if(machineNumber > playerNumber){                  // Condizione di verifica vincitore
     alert('Ha vinto la macchina')
     confronto = 'Ha vinto la macchina con il numero '+ machineNumber + ' e tu hai perso con il numero ' + playerNumber;
 }else{
@@ -16,4 +19,4 @@ if(machineNumber > playerNumber){
     confronto = 'Hai vinto tu con il numero '+ playerNumber  + ' e la macchina ha perso con il numero ' + machineNumber;
 }
 
-document.getElementById('risultato').innerHTML = confronto; 
+document.getElementById('risultato').innerHTML = confronto;  // Stampa a monitor del risultato
